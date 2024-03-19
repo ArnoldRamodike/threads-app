723cd9614dc54fa852898714c36ca0c59333f9c1
ArnoldRamodike <riccoarnold23@gmail.com>
2024-03-19 11:49:38 +0200

    Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,10 @@
-import {UserButton} from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <UserButton afterSignOurUrl="/"/> */}
-      <h1> Threads</h1>
+    <div className="">
+   
+      <h1 className="head-text text-left"> Home</h1>
     </div>
   );
 }
