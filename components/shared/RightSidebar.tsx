4f75878b71144631b002af645094c0da1fc49d33
ArnoldRamodike <riccoarnold23@gmail.com>
2024-03-19 11:50:23 +0200

    Right Sidebar

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const RightSidebar = () => {
+  return (
+    <section className='custom-srollbar rightsidebar flex-col justify-start'>
+        <div className="flex flex-1 ">
+            <h3 className='text-heading4-medium text-light-1'>
+                Suggested Comminities
+            </h3>
+        </div>
+        <div className="flex flex-1 ">
+            <h3 className='text-heading4-medium text-light-1'>
+                Suggested Users
+            </h3>
+        </div>
+    </section>
+  )
+}
+
+export default RightSidebar
