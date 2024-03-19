e9b719c4be35fba0df1e4a1bdfd088b0e79cf185
ArnoldRamodike <riccoarnold23@gmail.com>
2024-03-19 10:34:52 +0200

    auth layout and onbording Page

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,9 @@
+async function Page() {
+    return(
+        <main>
+            <h1 className="">On bording</h1>
+        </main>
+    )
+}
+
+export default Page;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import {UserButton} from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* <UserButton afterSignOurUrl="/"/> */}
+      <h1> Threads</h1>
+    </div>
+  );
+}
