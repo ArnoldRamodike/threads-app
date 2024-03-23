233ed5dce881d5687f79800d2d135bbe2ccb52ad
ArnoldRamodike <riccoarnold23@gmail.com>
2024-03-23 13:59:05 +0200

    Getting posts by id and all functionality

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -13,6 +13,71 @@ interface Params{
     path: string,
 }
 
+export async function fetchPosts(pageNumber =1, pagesize =20){
+    try {
+        connectToDb();
+
+        const skipAmount = (pageNumber -1) * pagesize;
+
+        const postQuery =  Thread.find({ parentId: {$in: [null, undefined]}})
+        .sort({createdAt: 'desc'})
+        .skip(skipAmount)
+        .limit(pagesize)
+        .populate({path: 'author', model: User})
+        .populate({path: 'children', populate:{
+            path: 'author', model: User, select: "_id name parentId image"
+        }})
+
+        const totalPostCount = await Thread.countDocuments({ parentId: {$in: [null, undefined]} })
+
+        const posts = await postQuery.exec();
+
+        const isNext = totalPostCount > skipAmount + posts.length;
+
+        return {posts, isNext}
+
+    } catch (error: any) {
+        return new Error(`failed to fetch Posts: ${error.message}`);
+    }
+}
+
+export async function fetchThreadById(id:string){
+    connectToDb();
+
+    try {
+        //Polulate Community
+
+        const thread = await Thread.findById(id)
+            .populate({
+                path: 'author',
+                model: User,
+                select: "_id id name image"
+            })
+            .populate({
+                path: 'children',
+                populate: [
+                    {
+                        path: 'author',
+                        model: User,
+                        select: "_id id name parentId image"  
+                    },
+                     {
+                        path: 'children',
+                        model: Thread,
+                        populate: {
+                            path: 'author',
+                            model: User,
+                            select: "_id id name parentId image" 
+                         }
+                     }
+            ]
+            }).exec();
+        return thread;
+    } catch (error: any) {
+        return new Error(`failed to fetch Post by Id: ${error.message}`);
+    }
+}
+
 export async function createThread({text, author, communityId, path}: Params){
     
     try {
@@ -21,7 +86,7 @@ export async function createThread({text, author, communityId, path}: Params){
         const createThread = await Thread.create({
             text, 
             author,
-            community: communityId = null,
+            community: null,
     
         });
     
