b366a26d8e1a0506278a9ee929c8fd200c3940fe
ArnoldRamodike <riccoarnold23@gmail.com>
2024-03-23 14:00:20 +0200

    getting all posts on Home pge

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,38 @@
+import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchPosts } from "@/lib/actions/thread.actions";
 import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+
+  const results = await fetchPosts(1, 30);
+  const user = await currentUser();
+  
   return (
-    <div className="">
-   
+    <>
       <h1 className="head-text text-left"> Home</h1>
-    </div>
+
+      <section className="mt-9 flex flex-col gap-10">
+        { results?.posts?.length === 0 ? (
+          <p className="no-result">No Threads found</p>
+        ) : (
+          <>
+          {results?.posts?.map((post) => (
+            <ThreadCard 
+              key={post.id}
+              id={post._id}
+              currentUserId= {user?.id || ""}
+              parentId={post.parentId}
+              content={post.text}
+              author={post.athor}
+              community={post.community}
+              createdAt={post.createdAt}
+              comments={post.comments}
+            />
+          ))}
+          </>
+        )}
+      </section>
+    </>
   );
 }
