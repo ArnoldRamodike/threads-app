27d1c28401632fa1273b1d45156671616be36bc1
ArnoldRamodike <riccoarnold23@gmail.com>
2024-03-26 20:45:10 +0200

    Comminities init

---
+++ app/(root)/communities/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = async () => {
+  return (
+    <section>
+        <h1 className='head-text'>Communities</h1>
+    </section>
+  )
+}
+
+export default page
