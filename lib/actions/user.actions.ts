537845e5a5dd485adaf74e4cb95a34f1f06934f5
ArnoldRamodike <riccoarnold23@gmail.com>
2024-03-26 20:44:03 +0200

    Get User Activity action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -122,3 +122,31 @@ export async function fetchUsers({
     throw new Error(`Faliled to fetch users: ${error.message}`);
   }
 }
+
+
+export async function getActivity(userId: string){
+  try {
+
+    connectToDb();
+    
+    const userThreads = await Thread.find({author: userId});
+
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.childrren)
+    }, []) 
+
+    const replies = await Thread.find({
+      _id: {$in: childThreadIds},
+      author: {$ne: userId}
+    }).populate({
+      path: 'author',
+      model: User,
+      select: 'name, image _id'
+    })
+
+    return replies;
+    
+  } catch (error: any) {
+    throw new Error(`Faliled to get Activity: ${error.message}`);
+  }
+}
